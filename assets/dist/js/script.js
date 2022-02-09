88f5d5e1d0b1c0cbed0d902f8e2a910c73416c87
Andrew <andrewmgunn26@gmail.com>
2022-02-09 11:47:16 -0600

    done kinda

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -10,6 +10,34 @@
         $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
     });
 
+    var i = 1;
+    $('#newRow').click(function() {
+        $('#edu').clone().appendTo('#eduNext').prop('id', 'edunew' + i);
+        i++;
+        $('#edunew'+i + '> input', $(this)).each(function(index) {
+            this.innerHTML = "";
+        })
+    });
+    var j = 1;
+    $('#newJob').click(function() {
+        $('#job').clone().appendTo('#jobNext').prop('id', 'jobnew' + j);
+        i++;
+        $('#jobnew'+i + '> input', $(this)).each(function(index) {
+            this.innerHTML = "";
+        })
+    });
+    $('#newRef').click(function() {
+        $('#ref2').clone().appendTo('#refNext').prop('id', 'refnew' + j);
+        i++;
+        $('#refnew'+i + '> input', $(this)).each(function(index) {
+            this.innerHTML = "";
+        })
+    });
+    // $('#newRow').click(function() {
+    //     $('#eduNext').innerHTML += '< div className = "form-row" id = "edu"'+i+'><div className = "form-group col-md-6" ><label className = "col-form-label" htmlFor = "inputCity" > School < /label> <input className="form-control" id="schoolInput" name="firstname" placeholder="John"type="text"> </div> <div className="form-group col-md-3"> <label className="col-form-label" htmlFor="enrolledInput">Enrolled</label> <input className="form-control" id="enrolledInput" name="enrolledInput" type="date"value="2022-02-09"> </div> <div className="form-group col-md-3"> <label className="col-form-label" htmlFor="gradInput">Graduated</label> <input className="form-control" id="gradInput" name="gradInput" type="date"value="2022-02-09"> </div> </div';
+    //
+    // });
+
     window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
